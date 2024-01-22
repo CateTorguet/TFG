0bf9866d99f81ec0d1d3497c7d19348d77155b25
Pablocastillot <114137161+Pablocastillot@users.noreply.github.com>
2024-01-22 19:35:53 +0100

    Dibujar rectangulos de areas

--- scriptIO.py
+++ scriptIO.py
@@ -2,48 +2,59 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import random
 
-def punto_dentro_del_rectangulo(coordenadas_rectangulo, punto):
-    x1, y1, x2, y2 = coordenadas_rectangulo
+def punto_dentro_del_rectangulo(coordenadas_rectangulo, punto, i):
+    x1, y1, x2, y2 = coordenadas_rectangulo[i]
     x, y = punto
     if x2 <= x <= x1 and y2 <= y <= y1:
         return True
     else:
         return False
 
-# Coordenadas del rectángulo
-coordenadas_rectangulo = (0.557083, 0.551111, 0.0291667, 0.0266667)
+def leer_archivo(nombre_archivo):
+    try:
+        with open(nombre_archivo, 'r') as archivo:
+            lineas = archivo.readlines()
+            coordenadas = []                                        # Cargar línea a línea 
 
-# Punto aleatorio
-punto_aleatorio = (random.uniform(0, 1), random.uniform(0, 1))
+            for i in range(len(lineas)):
+                lineas[i] = lineas[i].replace(',','')
 
-# Verificar si el punto está dentro del rectángulo
-resultado = punto_dentro_del_rectangulo(coordenadas_rectangulo, punto_aleatorio)
+            coordenadas = [tuple(map(float, linea.strip().split()[1:])) for linea in lineas]
 
-# Crear el gráfico
-fig, ax = plt.subplots()
+            return coordenadas                                      #[(0,0,0,0),(0,0,0,0),...]
+    except FileNotFoundError:
+        return f"El archivo '{nombre_archivo}' no fue encontrado."
+    except Exception as e:
+        return f"Ocurrió un error: {e}"
 
-# Dibujar el rectángulo
-rectangulo = patches.Rectangle((coordenadas_rectangulo[0], coordenadas_rectangulo[1]),
-                               coordenadas_rectangulo[2] - coordenadas_rectangulo[0],
-                               coordenadas_rectangulo[3] - coordenadas_rectangulo[1],
-                               linewidth=1, edgecolor='r', facecolor='none')
 
-ax.add_patch(rectangulo)
 
-# Marcar el punto aleatorio
-ax.plot(punto_aleatorio[0], punto_aleatorio[1], 'bo', label='Punto Aleatorio')
+nombre_archivo = 'coordenadas.txt'
+coordenadas_rectangulo = leer_archivo(nombre_archivo)
+i=0
 
-# Ajustes del gráfico
-ax.set_xlim(0, 1)  # Ajusta según las coordenadas x que necesites
-ax.set_ylim(0, 1)  # Ajusta según las coordenadas y que necesites
-ax.set_aspect('equal', adjustable='datalim')
+while True:                                                                     #Añadir for con i
+    punto_aleatorio = (random.uniform(0, 1), random.uniform(0, 1))                                             # Punto aleatorio
+    
+    resultado = punto_dentro_del_rectangulo(coordenadas_rectangulo, punto_aleatorio, i)                        # Verificar si el punto está dentro del rectángulo
+    # Crear el gráfico
+    fig, ax = plt.subplots()
+    rectangulo = patches.Rectangle((coordenadas_rectangulo[i][0], coordenadas_rectangulo[i][1]),
+                                coordenadas_rectangulo[i][2] - coordenadas_rectangulo[i][0],
+                                coordenadas_rectangulo[i][3] - coordenadas_rectangulo[i][1],
+                                linewidth=1, edgecolor='r', facecolor='none')
+    ax.add_patch(rectangulo)
+    ax.plot(punto_aleatorio[0], punto_aleatorio[1], 'bo', label='Punto Aleatorio')                                                                                                              # Ajustes del gráfico
+    ax.set_xlim(-0.5, 1.5)  
+    ax.set_ylim(-0.5, 1.5) 
+    ax.set_aspect('equal', adjustable='datalim')
+                                                                                                                # Mostrar leyenda y el gráfico
+    plt.legend()
+    plt.show()
 
-# Mostrar leyenda y el gráfico
-plt.legend()
-plt.show()
+    i=i+1
 
-# Imprimir el resultado
-if resultado:
-    print("El punto está dentro del rectángulo.")
-else:
-    print("El punto está fuera del rectángulo.")
+    if resultado:                                                                                               # Imprimir el resultado
+        print("El punto está dentro del rectángulo.")
+    else:
+        print("El punto está fuera del rectángulo.")
--- scriptRead.py
+++ scriptRead.py
@@ -1,10 +1,12 @@
-def leer_coordenadas_desde_archivo(nombre_archivo):
+def leer_coordenadas_desde_archivo(nombre_archivo): #introducir nº de líneas para bucle for
     try:
         with open(nombre_archivo, 'r') as archivo:
             # Lee la línea y divide las coordenadas
             linea = archivo.readline().strip()
-            coordenadas = list(map(float, linea.split()))
-            return tuple(coordenadas)
+            linea = linea.replace(',', '')                 #Quitarle la coma para manejarlo como float
+            coordenadas = list(map(float, linea.split()))         
+            return tuple(coordenadas[1:])
+        
     except FileNotFoundError:
         print(f"Error: El archivo {nombre_archivo} no existe.")
         return None
@@ -12,14 +14,26 @@ def leer_coordenadas_desde_archivo(nombre_archivo):
         print(f"Error al leer las coordenadas desde {nombre_archivo}: {e}")
         return None
 
-# Nombre del archivo de texto
+def leer_archivo(nombre_archivo):
+    try:
+        with open(nombre_archivo, 'r') as archivo:
+            lineas = archivo.readlines()
+            coordenadas = []                                        # Cargar línea a línea 
+
+            for i in range(len(lineas)):
+                lineas[i] = lineas[i].replace(',','')
+
+            coordenadas = [tuple(map(float, linea.strip().split()[1:])) for linea in lineas]
+
+            return coordenadas                                      #[(0,0,0,0),(0,0,0,0),...]
+    except FileNotFoundError:
+        return f"El archivo '{nombre_archivo}' no fue encontrado."
+    except Exception as e:
+        return f"Ocurrió un error: {e}"
+
 nombre_archivo = 'coordenadas.txt'
-
-# Lee las coordenadas desde el archivo
-coordenadas_rectangulo = leer_coordenadas_desde_archivo(nombre_archivo)
-
-# Verifica si las coordenadas se leyeron correctamente
+coordenadas_rectangulo = leer_archivo(nombre_archivo)
 if coordenadas_rectangulo is not None:
-    print(f"Coordenadas del rectángulo leídas desde {nombre_archivo}: {coordenadas_rectangulo}")
+    print(f"Coordenadas del rectángulo leídas desde {nombre_archivo}: {coordenadas_rectangulo}") 
 else:
     print("No se pudieron leer las coordenadas.")
